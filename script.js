42d2a4c84cfa92767cacbc20c92b7479d8be05a5
Ibijolaade <134616411+Ibijolaade@users.noreply.github.com>
2023-12-05 11:56:25 +0100

    final editing

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ const change = document.getElementById("rabbit");
 
 change.addEventListener("click", (event) => {
     event.target.style.background = "red";
-    event.target.style,color = "white";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("city");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "black";
+});
+
+const change3 = document.getElementById("creeper");
+change3.addEventListener("click",(event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("kayaking");
+change4.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "grey";
 });
