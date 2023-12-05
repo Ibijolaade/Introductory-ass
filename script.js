edd2b830775b8467164711d488f01fa9f7a8ffbb
Ibijolaade <134616411+Ibijolaade@users.noreply.github.com>
2023-12-05 11:30:39 +0100

    JS style

---
+++ script.js
@@ -0,0 +1,6 @@
+const change = document.getElementById("rabbit");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style,color = "white";
+});
